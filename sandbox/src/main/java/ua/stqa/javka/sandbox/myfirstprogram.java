7230542e298c8f0ff68e24ed1432ad23cd8985a1
Denis Komkov <denis.komkov@nixsolutions.com>
2019-03-18 14:33:17 +0200

    functions excercises

--- sandbox/src/main/java/ua/stqa/javka/sandbox/myfirstprogram.java
+++ sandbox/src/main/java/ua/stqa/javka/sandbox/myfirstprogram.java
@@ -2,7 +2,36 @@ package ua.stqa.javka.sandbox;
 
 public class MyFirstProgram {
 
+    public static void hello(String somebody) {
+        System.out.println("Hello, " + somebody + "!");
+    }
+
+    public static double area(double l) {
+        return l * l;
+    }
+
+    public static double area(double a, double b) {
+        return a * b;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+
+        hello("world");
+        hello("zaloopa");
+        hello("Alexei");
+
+        double l = 5;
+        System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+
+        double a = 4;
+        double b = 6;
+
+        System.out.println("Площадь прямоугла со сторонами " + a + " и " + b " = " + area(a, b));
+
+        // System.out.println("Hello, "+ somebody);
+        // String somebody = "world!";
+        // double l = 8.0;
+        // double s = 1 * 1;
+        // System.out.println("Площадь квадрата со стороной " + 1 + " = " + s);
     }
 }
