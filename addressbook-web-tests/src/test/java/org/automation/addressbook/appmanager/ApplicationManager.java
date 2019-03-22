9079de17622ef67409d202702d80586678e9ed1a
Denis Komkov <denis.komkov@nixsolutions.com>
2019-03-22 12:09:35 +0200

    minor changes.

--- addressbook-web-tests/src/test/java/org/automation/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/org/automation/addressbook/appmanager/ApplicationManager.java
@@ -1,7 +1,12 @@
 package org.automation.addressbook.appmanager;
 
 import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.opera.OperaOptions;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -10,9 +15,22 @@ public class ApplicationManager {
     private GroupHelper groupHelper;
     private SessionHelper sessionHelper;
     WebDriver driver;
+    private String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
     public void init() {
-        driver = new FirefoxDriver();
+
+        if (browser.equals(BrowserType.FIREFOX)) {
+            driver = new FirefoxDriver();
+        } else if (browser.equals(BrowserType.CHROME)) {
+            driver = new ChromeDriver();
+        } else if (browser.equals(BrowserType.IE)) {
+            driver = new InternetExplorerDriver();
+        }
+
         driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         driver.get("http://localhost/addressbook/group.php");
         groupHelper = new GroupHelper(driver);
--- addressbook-web-tests/src/test/java/org/automation/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/org/automation/addressbook/tests/TestBase.java
@@ -1,12 +1,16 @@
 package org.automation.addressbook.tests;
 
 import org.automation.addressbook.appmanager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import java.lang.*;
+
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager();
+
+    protected final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
 
     @BeforeMethod(alwaysRun = true)
     public void setUp() throws Exception {
