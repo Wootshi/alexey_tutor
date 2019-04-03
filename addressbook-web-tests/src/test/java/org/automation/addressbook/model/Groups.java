06098d08e96745aba885880f9bf2d623217eff1c
Denis Komkov <denis.komkov@nixsolutions.com>
2019-04-03 16:17:37 +0300

    Hamcrest in process. #53

--- addressbook-web-tests/src/test/java/org/automation/addressbook/model/Groups.java
+++ addressbook-web-tests/src/test/java/org/automation/addressbook/model/Groups.java
@@ -1,4 +1,20 @@
 package org.automation.addressbook.model;
 
-public class Groups {
+import com.google.common.collect.ForwardingSet;
+
+import java.util.Set;
+
+public class Groups extends ForwardingSet<GroupData> {
+
+    private Set<GroupData> delegate;
+
+    @Override
+    protected Set<GroupData> delegate() {
+        return delegate();
+    }
+
+    public Groups withAdded(GroupData group) {
+
+
+    }
 }
